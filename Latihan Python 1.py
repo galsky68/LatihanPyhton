866a76a234330cbef0119443dc9e17f5ed6b41b1
Galang Rintang W.P <galangsky6@gmail.com>
2020-10-26 21:46:28 +0700

    Tugas Python 1

---
+++ Latihan Python 1.py
@@ -0,0 +1,16 @@
+salam = "Assalamu'alaikum"
+namalengkap = "Galang Rintang Widya Pratama"
+namapanggilan = "Galang "
+NPM = ("312010299 ")
+tempatdantanggallahir = ("Bekasi, 6 Agustus 2001 ")
+umur = ("19 ")
+telepon = ("082210548806 ")
+alamat = ("Jl. Jaguar Raya No.28 ")
+terimakasih = "Thank you"
+print (salam)
+print ("Let me introduce my self. My name is" , namalengkap +
+       ", but you can call me" ,namapanggilan+ ". My NPM is" , NPM +
+       ". I was born in" ,tempatdantanggallahir+ "and I am" ,umur+
+       "years old. I am very glad if you want to invite my house in" ,alamat+
+       ". So, don't forget to call me before with the number" ,telepon)
+print (terimakasih)
